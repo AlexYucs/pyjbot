cc3934d56bacd1dc00e4ecd85dc0f81c8525dc9c
Alexrfy <peihaoy@gmail.com>
2016-07-25 16:30:15 -0400

    Update pyjbot.py

--- pyjbot.py
+++ pyjbot.py
@@ -41,11 +41,11 @@ def handle_messages():
     if u'value' in resp:
       if resp[u'value'] == "grocery":
         print("THIS GROCERY RIGHT HERE")
-        resp = client.run_actions('my-user-session-42',message, context0)
+        resp = client.converse('my-user-session-42',message, context0)
         print("This resp grocery ")
         print (resp)
         while('foodList' not in resp):
-          resp = client.run_actions('my-user-session-42',message, context0)
+          resp = client.converse('my-user-session-42',message, context0)
           print ("This resp ")
           print(resp)
         message = str(resp['foodList'])
