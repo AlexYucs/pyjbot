f85488b95c1b1c7a18efd7bb90efd692a327d3aa
Alexrfy <peihaoy@gmail.com>
2016-07-26 22:14:13 -0400

    Update pyjbot.py

--- pyjbot.py
+++ pyjbot.py
@@ -85,6 +85,21 @@ def handle_messages():
         send_message(PAT, sender, message)
         print("Probably sent")
         
+      elif resp[u'value'] == "talk":
+        print("This resp greetings RIGHT HERE")
+        # Create the kernel and learn AIML files
+        kernel = aiml.Kernel()
+        kernel.learn("std-startup.xml")
+        kernel.respond("load aiml b")
+
+        # Press CTRL-C to break this loop
+        while True:
+          print kernel.respond(raw_input("Enter your message >> "))
+          message = kernel.respond()
+          print("Trying to send...")
+          send_message(PAT, sender, message)
+          print("Probably sent")
+  
         #not working atm
       elif resp[u'value'] == "weather":
         #resp = client.run_actions('my-user-session-42',textmsg, context0)
